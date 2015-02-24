32ca76e0a114697372de7f79920d3591289beff9
Staffan Widegarn Åhlvik <staffan.tan@gmail.com>
2015-02-24 10:35:15 +0100

    fixed material leak

--- Scripts/VHSPostProcessEffect.cs
+++ Scripts/VHSPostProcessEffect.cs
@@ -3,15 +3,27 @@ using System.Collections;
 
 [RequireComponent (typeof(Camera))]
 public class VHSPostProcessEffect : PostEffectsBase {
-	Material m;
+	static Material m_Material = null;
+	protected Material material {
+		get {
+			if (m_Material == null) {
+				m_Material = new Material(shader);
+				m_Material.SetTexture("_VHSTex", VHS);
+				m_Material.hideFlags = HideFlags.DontSave;
+			}
+			return m_Material;
+		} 
+	}
+
 	public Shader shader;
 	public MovieTexture VHS;
 
 	float yScanline, xScanline;
 
 	public override void Start() {
-		m = new Material(shader);
-		m.SetTexture("_VHSTex", VHS);
+		//m = new Material(shader);
+		//m.SetTexture("_VHSTex", VHS);
+		//m.hideFlags = HideFlags.DontSave;
 		VHS.loop = true;
 		VHS.Play();
 	}
@@ -26,8 +38,14 @@ public class VHSPostProcessEffect : PostEffectsBase {
 		if(xScanline <= 0 || Random.value < 0.05){
 			xScanline = Random.value;
 		}
-		m.SetFloat("_yScanline", yScanline);
-		m.SetFloat("_xScanline", xScanline);
-		Graphics.Blit(source, destination, m);
+		material.SetFloat("_yScanline", yScanline);
+		material.SetFloat("_xScanline", xScanline);
+		Graphics.Blit(source, destination, material);
 	}
+
+	protected void OnDisable() {
+		if( m_Material ) {
+			DestroyImmediate( m_Material );
+		}
+	}	
 }
